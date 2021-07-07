0ebd589edfa3d45bc18accfc41bc43ec9053a68c
Gustavo Sartori <gustavo.sartori@tecnospeed.com.br>
2021-07-07 15:48:12 -0700

    Melhoria no log de erro do SQS

--- src/consumer/index.js
+++ src/consumer/index.js
@@ -13,7 +13,7 @@ const consumer = async () => {
 
     sqs.receiveMessage(params, async function (err, data) {
         if (err) {
-            console.log({ error: { message: err.message } });
+            console.log({ SQSError: { message: err.message } });
         } else if (data.Messages) {
             const cepId = data.Messages[0].Body;
             const receiptHandle = data.Messages[0].ReceiptHandle;
