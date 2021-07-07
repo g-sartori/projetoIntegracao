c2109b888a06c0baf284d991d59c4a18ad395abc
Gustavo Sartori <gustavo.sartori@tecnospeed.com.br>
2021-07-07 10:20:45 -0700

    Criação da rota POST /cep

--- src/producer/index.js
+++ src/producer/index.js
@@ -1,17 +1,27 @@
 import './setup/db'
-
 import { server } from './setup/server'
-import { CEPModel } from './models/ceps'
+import { CreateCEPService } from './services/CreateCEPService';
+import { SendQueueMessageService } from './services/SendQueueMessageService';
 
-server.get('/users', async (req, res) => {
+server.post('/cep', async (req, res) => {
   try {
-    const users = await CEPModel.find()
-    res.send(users)
+    console.log('\n \n \nEntrou no post de /cep');
+    const { cep } = req.body;
+    const createCEPService = new CreateCEPService();
+    const newCEP = await createCEPService.execute({ req, res, cep });
+    console.log('Saiu do CreateCEPService\n');
+    if (newCEP.statusCode===201){
+      const sendMessageService = new SendQueueMessageService();
+      sendMessageService.execute(newCEP);
+      console.log('Saiu do SendQueueMessageService\n');
+    }
+    console.log('Terminou o POST de /cep\n\n\n\n')
   } catch (error) {
+    console.log('Deu erro no post de /cep\n', error);
     res.send(500, error)
   }
 })
 
 
-server.start(() => console.log('Started server'))
+server.start(() => console.log('Started server on %s:%s', process.env.SERVER_HOST, process.env.SERVER_PORT))
 
